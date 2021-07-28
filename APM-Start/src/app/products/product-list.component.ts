3495b9fa25509aa7dd25d3ccddcc0de639e4dda6
Jie Zhou (JAY) <jiz156@pitt.edu>
2021-07-28 14:57:06 -0400

    practice passing data from a Component Using @Output

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -61,4 +61,8 @@ export class ProductListComponent implements OnInit{
       this.listFilter='cart';
       console.log('In OnInit')
     }
+
+    onRatingClicked(message: string): void {
+      this.pageTitle = 'Product List' + message;
+    }
 }
