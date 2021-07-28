33209ea191420b5460d1cecaad93a136c0e49c65
Jie Zhou (JAY) <jiz156@pitt.edu>
2021-07-28 14:02:42 -0400

    practice nested component and passing data to nested component

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,16 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
+    StarComponent,
     ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   bootstrap: [AppComponent]
 })
