9c09ada71c666ee83f7adb7bdbaebd663607ee5b
Jie Zhou (JAY) <jiz156@pitt.edu>
2021-07-29 09:50:29 -0400

    retrieving data using http

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = '';
     
     private _listFilter: string = '';
     get listFilter(): string{
@@ -41,8 +42,14 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-      this.products = this.productService.getProducts();
-      this.filteredProducts = this.products;
+      this.productService.getProducts().subscribe({
+        next: products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+        },
+        error: err => this.errorMessage = err
+      });
+      
     }
 
     onRatingClicked(message: string): void {
